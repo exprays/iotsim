fe312dbacd48638d877f49ecfe0be60461574318
surya <suryakant.subudhi@outlook.com>
2025-02-08 03:01:42 +0530

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,21 @@
-import IoTMonitor from "@/components/iotmonitor";
-import Image from "next/image";
+"use client"
+
+import GridDistortion from "@/components/global/GridDistortion";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/monitor");
+  }
+
   return (
-    <IoTMonitor />
+    <div>
+      <button onClick={onClick}>
+        monitor
+      </button>
+    </div>
   );
 }
